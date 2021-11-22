e31e040aa3bd8eb446840ae4dd82618813d6c397
ubuntutester <christianlopez641c@gmail.com>
2021-11-22 13:10:59 -0500

    removed lines

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-{/*import logo from './logo.svg';*/}
 import catgun from './images/catgun.png'
 import './App.css';
 
@@ -11,7 +10,6 @@ function App() {
           Follow the cat's orders and nothing will happen.
         </p>
         <img src={catgun} className="lol-logo" alt="meowgun" height={300} width={300}/>
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <p>
           Edit <code>src/App.js</code> and save to reload. re
         </p>
