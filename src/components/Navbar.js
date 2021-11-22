a7b4330ccd69777c861c638e610a4c99ebec771c
ubuntutester <christianlopez641c@gmail.com>
2021-11-22 17:07:06 -0500

    some testing for the icon nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLinkClickHandler } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' // imports the main icon thingy
-import { faCoffee } from '@fortawesome/free-solid-svg-icons' // imports the actual icon from the icon pkg thingy
+import { faCoffee, faBars, faTimes, fas } from '@fortawesome/free-solid-svg-icons' // imports the actual icon from the icon pkg thingy
 
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
     return (
        <>
         <nav className="navbar">
@@ -12,7 +15,12 @@ function Navbar() {
                 <Link to="/" className="navbar-log">
                     WOW
                 </Link>
-                <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon> 
+                <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
+                
+
+                <div className="menu-icon" onClick={handleClick}>
+                    <i className={click ? "fas faBars" : "fas faTimes"} />
+                </div> 
             </div> 
         </nav>
        </>
