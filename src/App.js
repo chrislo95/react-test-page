bbbeeabdaf3260da1d7238d30791e80b1f0cff3b
ubuntutester <christianlopez641c@gmail.com>
2021-11-22 12:18:14 -0500

    added cat gun pic test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import catgun from './images/catgun.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={catgun} className="lol-logo" alt="meowgun" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. re
