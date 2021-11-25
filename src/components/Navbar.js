c34859cd23f01cc1e60b372980b819bac0947452
test user <christianlopez641c@gmail.com>
2021-11-24 19:14:43 -0500

    added home item for nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { Link, useLinkClickHandler } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // imports the main icon thingy
-import { faCoffee, faBars, faTimes, fas } from '@fortawesome/free-solid-svg-icons'; // imports the actual icon from the icon pkg thingy
+import { faCoffee, faBars, faTimes, } from '@fortawesome/free-solid-svg-icons'; // imports the actual icon from the icon pkg thingy
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
 
@@ -9,6 +9,7 @@ function Navbar() {
     const [click, setClick] = useState(false);
 
     const handleClick = () => setClick(!click);
+    const closeMobileMenu= () => setClick(false);
     return (
        <>
         <nav className="navbar">
@@ -20,6 +21,13 @@ function Navbar() {
                 <div className="menu-icon" onClick={handleClick}>
                     <FontAwesomeIcon icon={click ? faTimes : faBars} />
                 </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav=item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                </ul> 
             </div> 
         </nav>
        </>
