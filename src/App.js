100988bd38c5750ad86e4e92c6b232fed71c139c
homepc <christianlopez641c@gmail.com>
2022-04-02 16:40:44 -0400

    added new png

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import catgun from './images/catgun.png';
+import chainsaw from '/images/chainsaw.PNG';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
@@ -22,6 +23,7 @@ function App() {
           Cat gun
         </p>
         <img src={catgun} className="lol-logo" alt="meowgun" height={300} width={300}/>
+        <img src={chainsaw} alt='animestuff' height={300} width={300}/>
       </header>
     </div>
   );
