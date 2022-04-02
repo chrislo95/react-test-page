60efe12ddb345ab9278b4175eac76eca1cc30fc6
homepc <christianlopez641c@gmail.com>
2022-04-02 17:21:05 -0400

    added new pics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import catgun from './images/catgun.png';
 import chainsaw from './images/chainsaw.PNG';
+import kara from './images/knk.png'
+import tsukihime from './images/tsukihime.png'
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
@@ -26,6 +28,8 @@ function App() {
 
         <p>Chainsaw</p>
         <img src={chainsaw} className="lol-logo" alt='animestuff' height={300} width={300}/>
+        <img src={kara} className="lol-logo" alt='animestuff' height={300} width={300}/>
+        <img src={tsukihime} className="lol-logo" alt='animestuff' height={300} width={300}/>
       </header>
     </div>
   );
