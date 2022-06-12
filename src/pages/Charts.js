9672ba6b9ebb31d4aca69e68b95a840411d68916
homepc <christianlopez641c@gmail.com>
2022-06-12 16:05:34 -0400

    added return thing to the charts pages

--- src/pages/Charts.js
+++ src/pages/Charts.js
@@ -4,13 +4,14 @@ import catgun from '../images/catgun.png';
 
 function Charts() {
 
-    <header className="App-header">
-        <p>
-          Catgun
-        </p>
-        <img src={catgun} className="lol-logo" alt="meowgun" height={300} width={300}/>
-    </header>
-
+    return (
+        <header className="App-header">
+            <p>
+            Catgun
+            </p>
+            <img src={catgun} className="lol-logo" alt="meowgun" height={300} width={300}/>
+        </header>
+    )
 }
 
 export default Charts;
